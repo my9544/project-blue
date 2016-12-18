71310fc27b11da8da92588801571908becde0bbf
my9544 <my9544@naver.com>
2016-12-18 20:33:15 +0900

    Update d-day.c

--- d-day.c
+++ d-day.c
@@ -1,45 +1,86 @@
 #include <stdio.h>
 
 
-int month(int m, int leapt){
-	if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
-		return 31;
+int month(int gm, int hm, int leapt){
+	int m[12];
+	if(gm > hm){
+		while(gm != hm){
+			int i = 0;
+			gm--;
+			m[i] = gm;
+			i++; 
+		}
+		return m;	
 	}
-	if(m == 2){
-		if(leapt >= 1){
-			return 29;
-		}else{
-			return 28;
+	else if(hm > gm){
+		while(gm != hm){
+			int j = 0;
+			hm--;
+			m[i] = hm;
+			j++;
+		}
+		return m;
+	}
+	for(int d = 0; m[d] != '\0'; d++){
+		if(m[d] == 1 || m[d] == 3 || m[d] == 5 || m[d] == 7 || m[d] == 8 || m[d] == 10 || m[d] == 12){
+			return 31;
+		}
+		if(m[d] == 2){
+			if(leapt >= 1){
+				return 29;
+			}else{
+				return 28;
+			}
+		}
+	
+		if(m[d] == 4 || m[d] == 6 || m[d] == 9 || m[d] == 11){
+			return 30;
 		}
 	}
+
 	
-	if(m == 4 || m == 6 || m == 9 || m == 11){
-		return 30;
+}
+
+
+int dayc(int gday, int hday){
+	int day = 0;
+	if(gday > hday){
+		day = gday - hday;
 	}
-	
+	else if(gday < hday){
+		day = hday - gday;
+	}
+	return day;
 }
 
 
 
 
 
-
-int leap(int year){
+int leap(int hyear, int gyear){
 	int leapt = 0;
-	if((year % 4 == 0 && year % 100 == 0)|| year % 400 == 0){
-		leapt += 1;
-		return leapt;
+	while(gyear != hyear){
+		if((gyear % 4 == 0 && gyear % 100 == 0)|| gyear % 400 == 0){
+			leapt++;
+			gyear++;
+		}
 	}
+	return leapt;
 }
 
 
 int main(){
-	int m = 0;
-	int year = 0;
-	int leapt = 0;
-	scanf("%d %d", &m, &year);
+	int gm = 0;
+	int hm = 0;
+	int hyear = 0;
+	int gyear = 0;
+	int leap = 0;
+	int hday = 0;
+	int gday = 0;
+
+	scanf("%d %d %d %d %d %d", &gm, &gday, &gyear, &hm, &hday, &hyear);
 	
-	leapt += leap(year);
+	leap += leap(year);
 	
-	printf("%d", month(m, leapt));
+	
 }
